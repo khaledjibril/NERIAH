ce7eeaeaddf444cae32c5d94d96bd9b7254040e8
Sammy-blaq <abdullahiabdussamad00@gmail.com>
2025-10-31 23:07:17 +0100

    Made Adjustment to the field gap and set link path

--- pages/SignIn.jsx
+++ pages/SignIn.jsx
@@ -3,6 +3,10 @@
 // COMPONENTS
 import Header from "../src/components/Header";
 import Footer from "../src/components/Footer";
+import { Link } from "react-router-dom";
+
+// ICONS
+import { CiLogin } from "react-icons/ci";
 
 const SignIn = () => {
   return (
@@ -14,7 +18,6 @@ const SignIn = () => {
         className="main-content flex flex-col items-center justify-center flex-1 w-full h-full"
         id="top"
       >
-        {/* SIGNIN FORM */}
         <section className="flex items-center justify-center w-full h-full">
           <div className="flex flex-col items-center justify-center w-200 mx-6 rounded-lg bg-[#f7f7e3] shadow-xl border border-[#ddddb5]">
             <div className="flex flex-col p-[2.4rem] gap-8 text-center my-8">
@@ -26,8 +29,9 @@ const SignIn = () => {
               </p>
             </div>
             <div className="p-[2.4rem] pt-0 w-full">
+              {/* SIGNIN FORM */}
               <form action="" class="space-y-6 w-full">
-                <div className="space-y-2">
+                <div className="mb-16">
                   <label
                     for="eamil"
                     className="capitalize text-[#504230] font-medium"
@@ -41,7 +45,7 @@ const SignIn = () => {
                     id="eamil"
                   />
                 </div>
-                <div className="space-y-2 mt-[2.4rem]">
+                <div className="space-y-2 mt-[2.4rem] mb-16">
                   <div class="flex justify-between items-center">
                     <label
                       for="password"
@@ -49,40 +53,40 @@ const SignIn = () => {
                     >
                       password
                     </label>
-                    <a
-                      href="#"
+                    <Link
+                      to="#"
                       className="capitalize text-[#8a775c] text-[1.4rem] hover:text-[#8a775c]/60 transition-colors duration-300 ease-out"
                     >
                       forgot your password?
-                    </a>
+                    </Link>
                   </div>
                   <input
                     type="password"
-                    className="flex text-[1.4rem] border border-[#ddddb5] bg-[#f5f5dc] rounded-lg p-4 w-full mt-4 placeholder:text[#8a775c]"
+                    className="flex text-[1.4rem] border border-[#ddddb5] bg-[#f5f5dc] rounded-lg p-4 w-full mt-4 placeholder:text[#8a775c] "
                     placeholder="********"
                     id="password"
                   />
                 </div>
                 <button
                   type="button"
-                  className="flex items-center justify-center gap-4 text-white bg-[#a68b64] px[1rem] py-6 rounded-md mt-[2.4rem] w-full"
+                  className="flex items-center justify-center gap-4 text-white bg-[#a68b64] px[1rem] py-6 rounded-md mt-[2.4rem] w-full mb-16 cursor-pointer hover:bg-[#a68b64]/80 transition-all duration-300 ease-in-out"
                 >
-                  <ion-icon name="log-in" class="text-[1.6rem]"></ion-icon>
+                  <CiLogin className="text-[1.6rem]" />
                   Sign In
                 </button>
               </form>
-              <div className="mt-[2.4rem]">
+              <div className="my-[2.4rem]">
                 <div className="flex items-center justify-center">
                   <p className="text-[#8a775c]">Or continue with</p>
                 </div>
-                <div className="flex justify-center items-center mt-[2.4rem] text-[#8a775c]">
+                <div className="flex justify-center items-center mt-16 text-[#8a775c]">
                   <p>Don't have account?</p>
-                  <a
-                    href="./sign-up.html"
-                    className="font-semibold hover:text-[#8a775c]/60 transition-colors duration-300 ease-out"
+                  <Link
+                    to="/sign-up"
+                    className="font-semibold hover:text-[#8a775c]/60 transition-colors duration-300 ease-out focus-within:ring-2 focus-within:ring-primary focus-within:ring-offset-6"
                   >
                     Sign up
-                  </a>
+                  </Link>
                 </div>
               </div>
             </div>
--- pages/SignUp.jsx
+++ pages/SignUp.jsx
@@ -3,15 +3,130 @@
 // COMPONENTS
 import Header from "../src/components/Header";
 import Footer from "../src/components/Footer";
+import { Link } from "react-router-dom";
+
+// ICONS
+import { IoIosPersonAdd } from "react-icons/io";
 
 const SignUp = () => {
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
-      <main className="main-content flex flex-col flex-1" id="top">
-        <div className="container">
-          <p>Sign Up</p>
-        </div>
+
+      {/* MAIN CONTENT */}
+      <main
+        className="main-content flex flex-col items-center justify-center flex-1 w-full h-full"
+        id="top"
+      >
+        <section className="flex items-center justify-center w-full h-full">
+          <div className="flex flex-col items-center justify-center w-200 mx-6 rounded-lg bg-[#f7f7e3] shadow-xl border border-[#ddddb5] my-24">
+            <div className="flex flex-col p-[2.4rem] gap-8 text-center my-8">
+              <h1 className="capitalize text-[#504230] text-[3rem] font-bold">
+                Create an Account
+              </h1>
+              <p className="text-[#8a775c] text-[1.4rem] mt-3">
+                Join our community of photography lovers.
+              </p>
+            </div>
+            <div className="p-[2.4rem] pt-0 w-full">
+              {/* SIGNUP FORM */}
+              <form action="" class="space-y-6 w-full">
+                <div className="mb-16">
+                  <label
+                    for="full-name"
+                    className="capitalize text-[#504230] font-medium"
+                  >
+                    Full Name
+                  </label>
+                  <input
+                    type="tex"
+                    className="flex text-[1.4rem] border border-[#ddddb5] bg-[#f5f5dc] rounded-lg p-4 w-full mt-4 placeholder:text[#8a775c]"
+                    placeholder="John Doe"
+                    id="full-name"
+                  />
+                </div>
+                <div className="space-y-2 mt-[2.4rem] mb-16">
+                  <label
+                    for="phone"
+                    className="capitalize text-[#504230] font-medium"
+                  >
+                    Phone Number
+                  </label>
+
+                  <input
+                    type="tel"
+                    className="flex text-[1.4rem] border border-[#ddddb5] bg-[#f5f5dc] rounded-lg p-4 w-full mt-4 placeholder:text[#8a775c] "
+                    placeholder="e.g., +1 (123) 456 789"
+                    id="phone"
+                  />
+                </div>
+                <div className="space-y-2 mt-[2.4rem] mb-16">
+                  <label
+                    for="email"
+                    className="capitalize text-[#504230] font-medium"
+                  >
+                    Email
+                  </label>
+
+                  <input
+                    type="email"
+                    className="flex text-[1.4rem] border border-[#ddddb5] bg-[#f5f5dc] rounded-lg p-4 w-full mt-4 placeholder:text[#8a775c] "
+                    placeholder="e.g., you@example.com"
+                    id="email"
+                  />
+                </div>
+                <div className="space-y-2 mt-[2.4rem] mb-16">
+                  <label
+                    for="password"
+                    className="capitalize text-[#504230] font-medium"
+                  >
+                    Password
+                  </label>
+
+                  <input
+                    type="password"
+                    className="flex text-[1.4rem] border border-[#ddddb5] bg-[#f5f5dc] rounded-lg p-4 w-full mt-4 placeholder:text[#8a775c] "
+                    placeholder="**************"
+                    id="password"
+                  />
+                </div>
+                <div className="space-y-2 mt-[2.4rem] mb-16">
+                  <label
+                    for="confirm-Password"
+                    className="capitalize text-[#504230] font-medium"
+                  >
+                    Confirm Password
+                  </label>
+
+                  <input
+                    type="passord"
+                    className="flex text-[1.4rem] border border-[#ddddb5] bg-[#f5f5dc] rounded-lg p-4 w-full mt-4 placeholder:text[#8a775c] "
+                    placeholder="Enter password again..."
+                    id="confirm-password"
+                  />
+                </div>
+                <button
+                  type="button"
+                  className="flex items-center justify-center gap-4 text-white bg-[#a68b64] px[1rem] py-6 rounded-md mt-[2.4rem] w-full mb-16 cursor-pointer hover:bg-[#a68b64]/80 transition-all duration-300 ease-in-out"
+                >
+                  <IoIosPersonAdd className="text-[1.6rem]" />
+                  Sign Up
+                </button>
+              </form>
+              <div className="my-[2.4rem]">
+                <div className="flex justify-center items-center mt-16 text-[#8a775c]">
+                  <p>Already have an account? </p>
+                  <Link
+                    to="/sign-in"
+                    className="font-semibold hover:text-[#8a775c]/60 transition-colors duration-300 ease-out focus-within:ring-2 focus-within:ring-primary focus-within:ring-offset-6"
+                  >
+                    Sign in
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
       </main>
       <Footer />
     </div>
